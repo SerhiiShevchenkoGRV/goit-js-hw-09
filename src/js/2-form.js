0a1dbd95bdfc8c3a782550410754a6039eda8d82
Serhii Shevchenko <sergey.groove.shevchenko@gmail.com>
2024-12-09 16:16:10 +0100

    add form script

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,36 @@
-console.log('Form');
+const formData = { email: '', message: '' };
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+
+const savedData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+if (savedData.email || savedData.message) {
+  formData.email = savedData.email || '';
+  formData.message = savedData.message || '';
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+  if (name === 'email' || name === 'message') {
+    formData[name] = value.trim();
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+  formData.email = '';
+  formData.message = '';
+});
